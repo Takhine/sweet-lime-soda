3177ef414b643d7e6e8f1c6e955c73e3e68b1f97
Aniketh <anikethnair97@gmail.com>
2020-04-22 11:47:46 +0530

    Layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Layout from "./Layout";
+import Layout from "./Layout/index";
 import Home from "pages/Home";
 
 function App() {
