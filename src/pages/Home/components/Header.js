17059f6e16c52bbdc1fb0b1006d00978aee5bbc3
Aniketh <anikethnair97@gmail.com>
2020-04-22 06:27:46 +0530

    Header Desktop

--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -7,35 +7,44 @@ import portrait from "static/images/portrait.png";
 export default function Header() {
   return (
     <div className="header">
-      <section className="left-col">
-        <div className="title">
-          <img className="title__image" src={title} alt="Sweet Lime Soda" />
-          <div className="navigation">
-            <ul className="navigation__list">
-              <li className="navigation__list__item">Print</li>
-              <li className="navigation__list__item">Motion</li>
-              <li className="navigation__list__item">About</li>
-              <li className="navigation__list__item">Contact</li>
-            </ul>
+      <div className="top-row">
+        <section className="left-col">
+          <div className="title">
+            <img className="title__image" src={title} alt="Sweet Lime Soda" />
+            <div className="navigation">
+              <ul className="navigation__list">
+                <li className="navigation__list__item">Print</li>
+                <li className="navigation__list__item">Motion</li>
+                <li className="navigation__list__item">About</li>
+                <li className="navigation__list__item">Contact</li>
+              </ul>
+            </div>
           </div>
-        </div>
-        <div className="landscape">
-          <img
-            className="landscape__image"
-            src={landscape}
-            alt="Bahurupt Sweet Lime Soda"
-          />
-        </div>
-      </section>
-      <section className="right-col">
-        <div className="portrait">
-          <img
-            className="portrait__image"
-            src={portrait}
-            alt="Bahurupt Sweet Lime Soda"
-          />
-        </div>
-      </section>
+          <div className="landscape">
+            <img
+              className="landscape__image"
+              src={landscape}
+              alt="Bahurupt Sweet Lime Soda"
+            />
+          </div>
+        </section>
+        <section className="right-col">
+          <div className="portrait">
+            <img
+              className="portrait__image"
+              src={portrait}
+              alt="Bahurupt Sweet Lime Soda"
+            />
+          </div>
+        </section>
+      </div>
+      <div className="bottom-row">
+        <article className="business">
+          <h2 className="business__message">
+            Specializing in high-end art buying and production services
+          </h2>
+        </article>
+      </div>
     </div>
   );
 }
